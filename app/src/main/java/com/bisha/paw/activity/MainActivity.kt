b71e9b46ab258c289b52e86f4e6ccbacd5201ab5
kireins <kireishafira@gmail.com>
2022-04-20 11:44:29 +0700

    vet details

--- app/src/main/java/com/bisha/paw/activity/MainActivity.kt
+++ app/src/main/java/com/bisha/paw/activity/MainActivity.kt
@@ -3,31 +3,13 @@ package com.bisha.paw.activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Switch
-import androidx.appcompat.app.AppCompatDelegate
-import com.bisha.paw.R
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         fun getLaunchService(from: Context) = Intent(from, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val switch: Switch = findViewById(R.id.btnSwitch)
-
-        switch.setOnCheckedChangeListener{ buttonView, isChecked ->
-            if (isChecked){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }else{
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
         }
     }
 }
