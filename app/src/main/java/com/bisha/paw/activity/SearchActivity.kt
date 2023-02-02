8ce16bd0d7193bebeb6de347fc1864f4b193ba54
ayeshaazeema <ayesha.azeema@gmail.com>
2023-02-02 11:31:06 +0700

    Search Activity

--- app/src/main/java/com/bisha/paw/activity/SearchActivity.kt
+++ app/src/main/java/com/bisha/paw/activity/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.bisha.paw.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bisha.paw.R
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
