712584d7c90537a403863f4768cc6223da58c930
ayeshaazeema <ayesha.azeema@gmail.com>
2023-02-14 20:08:05 +0700

    Vet + Detail + Spinner

--- app/src/main/java/com/bisha/paw/presentation/vet/VetAdapter.kt
+++ app/src/main/java/com/bisha/paw/presentation/vet/VetAdapter.kt
@@ -9,9 +9,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bisha.paw.R
 import com.bisha.paw.data.VetModel
+import com.bisha.paw.utils.toFormatRupiah
 
-class VetAdapter(var data: ArrayList<VetModel>, var context: Activity?) :
-    RecyclerView.Adapter<VetAdapter.MyViewHolder>() {
+class VetAdapter(
+    private val data: ArrayList<VetModel>,
+    private val onClick: (VetModel) -> Unit
+) : RecyclerView.Adapter<VetAdapter.MyViewHolder>() {
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val vetName = view.findViewById<TextView>(R.id.tvVetName)
@@ -19,6 +22,14 @@ class VetAdapter(var data: ArrayList<VetModel>, var context: Activity?) :
         val vetLocation = view.findViewById<TextView>(R.id.tvVetLocation)
         val vetPrice = view.findViewById<TextView>(R.id.tvVetPrice)
         val vetImage = view.findViewById<ImageView>(R.id.ivVet)
+
+        fun bind(model: VetModel) {
+            vetName.text = model.vetName
+            vetHospital.text = model.vetHospital
+            vetLocation.text = model.vetLocation
+            vetPrice.text = model.vetPrice.toFormatRupiah()
+            vetImage.setImageResource(model.vetImg)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -28,11 +39,10 @@ class VetAdapter(var data: ArrayList<VetModel>, var context: Activity?) :
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.vetName.text = data[position].vetName
-        holder.vetHospital.text = data[position].vetHospital
-        holder.vetLocation.text = data[position].vetLocation
-        holder.vetPrice.text = data[position].vetPrice
-        holder.vetImage.setImageResource(data[position].vetImg)
+        holder.bind(data[position])
+        holder.itemView.setOnClickListener {
+            onClick(data[position])
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/bisha/paw/presentation/vet/VetFragment.kt
+++ app/src/main/java/com/bisha/paw/presentation/vet/VetFragment.kt
@@ -30,57 +30,14 @@ class VetFragment : Fragment() {
 
         rvVet.apply {
             setHasFixedSize(true)
-            adapter = VetAdapter(VetArray, activity)
             layoutManager = linearLayoutManager
+            adapter = VetAdapter(VetModel.getVets()) {
+                VetDetailActivity.start(requireContext(), it)
+            }
         }
 
         return view
     }
-
-    val VetArray: ArrayList<VetModel>
-        get() {
-
-            val vetArray = ArrayList<VetModel>()
-
-            //1
-            val vet1 = VetModel()
-            vet1.vetName = "Anggi Anggini"
-            vet1.vetHospital = "North Animal Clinic"
-            vet1.vetLocation = "North Jakarta, Indonesia"
-            vet1.vetPrice = "Rp 200.000,00"
-            vet1.vetImg = R.drawable.anggi_anggini
-
-            //2
-            val vet2 = VetModel()
-            vet2.vetName = "Budi Budiman"
-            vet2.vetHospital = "East Animal Clinic"
-            vet2.vetLocation = "East Jakarta, Indonesia"
-            vet2.vetPrice = "Rp 180.000,00"
-            vet2.vetImg = R.drawable.budi_budiman
-
-            //3
-            val vet3 = VetModel()
-            vet3.vetName = "Cantik Cantika"
-            vet3.vetHospital = "West Animal Clinic"
-            vet3.vetLocation = "West Jakarta, Indonesia"
-            vet3.vetPrice = "Rp 150.000,00"
-            vet3.vetImg = R.drawable.cantik_cantika
-
-            //4
-            val vet4 = VetModel()
-            vet4.vetName = "Dani Daniel"
-            vet4.vetHospital = "South Animal Clinic"
-            vet4.vetLocation = "South Jakarta, Indonesia"
-            vet4.vetPrice = "Rp 120.000,00"
-            vet4.vetImg = R.drawable.dani_daniel
-
-            vetArray.add(vet1)
-            vetArray.add(vet2)
-            vetArray.add(vet3)
-            vetArray.add(vet4)
-
-            return vetArray
-        }
 
     override fun onDestroyView() {
         super.onDestroyView()
